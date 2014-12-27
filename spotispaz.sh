30485bd063a8c8d93bd74facb5f7674e1c189261
Tejas Manohar <tejasmanohar@users.noreply.github.com>
2014-12-27 05:35:12 -0600

    download if not there

--- spotispaz.sh
+++ spotispaz.sh
@@ -1,5 +1,7 @@
 function spotispaz {
+  git clone https://github.com/dronir/SpotifyControl.git ~/SpotifyControl
+  echo "alias spotify='osascript ~/SpotifyControl/SpotifyControl.scpt'" >> ~/.bash_profile
   while true; do
-    spotify play/pause
+      spotify play/pause
   done
 }
